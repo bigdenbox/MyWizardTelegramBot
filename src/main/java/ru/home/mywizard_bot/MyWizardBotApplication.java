5161e71e84baf81c4e42636d511abf0655a14301
bigdenbox <bigdenbox@gmail.com>
2020-08-24 00:20:19 +0300

    .project

--- src/main/java/ru/home/mywizard_bot/MyWizardBotApplication.java
+++ src/main/java/ru/home/mywizard_bot/MyWizardBotApplication.java
@@ -1,4 +1,4 @@
-package ru.home.mywizard_bot;
+package src.main.java.ru.home.mywizard_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
